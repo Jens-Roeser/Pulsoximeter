efa02805a7a8ce16efc5dce66f5d793bf65f0938
Jens Roeser <jens-roeser@outlook.de>
2018-05-12 09:49:56 +0200

    Master alters Projekt

---
+++ src/Alarm.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jens
+ */
+public class Alarm {
+    
+    public void spo2alarm(){
+        
+    }
+    
+    public void upper_hralarm(){
+        
+    }
+    
+    public void lower_hralarm(){
+        
+    }
+    
+    //Possible in surrveillance
+    public void alarmbreak(){
+        
+    }
+}
